7aaf9c41975c60ebd9d8a71ee51c7113c345681a
Miha <miha.skrjanec@gmail.com>
2025-05-29 14:32:06 +0200

    Project Setup

---
+++ app/components/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="show h-16 w-full border-b border-border">
+      <div className="show h-full max-w-3xl"></div>
+    </div>
+  )
+}
