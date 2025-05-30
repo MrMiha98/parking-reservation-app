35829fe6ccc33f552b16f6e776448a64d179cf10
Miha <miha.skrjanec@gmail.com>
2025-05-30 15:56:48 +0200

    hero page

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,13 +5,13 @@ export default function Footer() {
   return (
     <footer className="w-full py-10">
       <div className="mx-auto px-4 sm:px-6 max-w-3xl flex justify-between">
-        <Link href="https://github.com/MrMiha98/parking-reservation-app" className="flex flex-row items-center gap-x-2 cursor-pointer">
+        <Link href="https://github.com/MrMiha98/parking-reservation-app" target="_blank" className="flex flex-row items-center gap-x-2 cursor-pointer">
           <p className="font-normal text-sm text-primary underline underline-offset-4 dark:text-darkmode-primary">Source on GitHub</p>
           <SquareArrowOutUpRight size="16" className="text-primary dark:text-darkmode-primary"/>
         </Link>
         <div className="flex flex-row items-center gap-x-2 cursor-pointer">
           <p className="font-normal text-sm text-primary dark:text-darkmode-primary">Website hosted by:</p>
-          <Link href="https://vercel.com" className="flex flex-row items-center gap-x-2">
+          <Link href="https://vercel.com" target="_blank" className="flex flex-row items-center gap-x-2">
             <p className="font-normal text-sm text-primary dark:text-darkmode-primary flex flex-row items-center gap-x-2 hover:underline underline-offset-4">Vercel™</p>
             <ScreenShare size="16" className="text-primary dark:text-darkmode-primary"/>
           </Link>
--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function Hero() {
   return (
-    <main className="flex-grow"></main>
+    <main className="w-full max-w-3xl mx-auto flex-grow p-4 sm:p-6">
+      <div className="show w-full flex flex-row justify-between self-start">
+        <div className="w-[55%] flex flex-col gap-y-6">
+          <h1 className="font-bold text-primary text-5xl leading-16">Reserve Your Car Parking Spot</h1>
+          <p className="text-[#323232]">Reserve your spot ahead of time and know exactly where to park before you leave home.</p>
+          <div>Melinda Smith: "nsdj sjdb jasndk askd nask nda"</div>
+          <Button className="w-full h-14 cursor-pointer font-bold text-lg rounded-lg">Reserve Now</Button>
+        </div>
+        <Image src="/media/parking-grid.png" width={256} height={256} alt="Car Parking Grid Layout" className="object-fit"/>
+      </div>
+    </main>
   );
 }
