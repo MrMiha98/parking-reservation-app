7a915280ad4c6670a98f8d43f4763f4f48bf7ad3
Miha <miha.skrjanec@gmail.com>
2025-05-30 18:49:12 +0200

    Hero done

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,17 +1,18 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { BadgeCheck } from "lucide-react";
 
 export default function Hero() {
   return (
     <main className="w-full max-w-3xl mx-auto flex-grow p-4 sm:p-6">
-      <div className="show w-full flex flex-row justify-between self-start">
-        <div className="w-[55%] flex flex-col gap-y-6">
-          <h1 className="font-bold text-primary text-5xl leading-16">Reserve Your Car Parking Spot</h1>
-          <p className="text-[#323232]">Reserve your spot ahead of time and know exactly where to park before you leave home.</p>
-          <div>Melinda Smith: "nsdj sjdb jasndk askd nask nda"</div>
-          <Button className="w-full h-14 cursor-pointer font-bold text-lg rounded-lg">Reserve Now</Button>
+      <div className="w-full flex flex-col md:flex-row justify-between">
+        <div className="w-full md:w-1/2 flex flex-col">
+          <h1 className="font-bold text-black dark:text-white text-[42px] leading-12">Reserve Your Car Parking Spot</h1>
+          <p className="mt-3 text-[#323232] dark:text-[#d2d2d2]">Reserve your spot ahead of time and know exactly where to park before you leave home.</p>
+          <div className="mt-6 text-primary dark:text-darkmode-primary">"Fast booking, super easy! Parked stress-free, Will use again!" - Melinda Smith <BadgeCheck strokeWidth="3px" size="16" color="#1da1f2" className="inline align-middle mb-1"/></div>
+          <Button className="mt-4 h-12 cursor-pointer dark:bg-white dark:hover:bg-[#c8c8c8]">Reserve Now</Button>
         </div>
-        <Image src="/media/parking-grid.png" width={256} height={256} alt="Car Parking Grid Layout" className="object-fit"/>
+        <Image src="/media/phone-click.png" alt="Parking Spot Grid Layout"  width={1000} height={1000} className="aspect-square object-fill rounded-2xl w-72 mt-8 mx-auto md:w-[45%] md:mt-0"/>
       </div>
     </main>
   );
