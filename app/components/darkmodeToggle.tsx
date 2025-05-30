30e921723fed6d4353a252eff9ef4d95f705ff39
Miha <miha.skrjanec@gmail.com>
2025-05-30 14:02:42 +0200

    darkmode perfected

---
+++ app/components/darkmodeToggle.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Sun, Moon } from "lucide-react"
+import { useState, useEffect } from "react"
+
+export default function DarkmodeToggle () {
+  const [darkmode, setDarkmode] = useState(false);
+
+  function handleClick() {
+    setDarkmode((prevDarkmode) => !prevDarkmode);
+  }
+
+  useEffect(() => {
+    const html = document.documentElement;
+    
+    if (darkmode) {
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+    }
+  }, [darkmode]);
+
+  return (
+    <button onClick={handleClick} className="cursor-pointer transition-all ease-in duration-150 hover:scale-105">
+      {darkmode ? <Sun className="text-yellow-500" /> : <Moon className="text-[#424c55]" />}
+    </button>
+  )
+}
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import DarkmodeToggle from "./darkmodeToggle"
 
 export default function Navbar() {
   return (
@@ -8,14 +9,18 @@ export default function Navbar() {
       <div className="h-16 max-w-3xl mx-auto flex items-center justify-between px-4 sm:px-6">
         <div className="h-16 flex items-center gap-4">
           <Link href="/">
-            <Image src="/media/parking-area.png" alt="App Logo" width={32} height={32} className="transition-all ease-in duration-150 hover:scale-105" />
+            <Image src="/media/logo-dark.png" alt="App Logo" width={42} height={42} className="dark:hidden transition-all ease-in duration-150 hover:scale-105"/>
+            <Image src="/media/logo-light.png" alt="App Logo" width={42} height={42} className="hidden dark:block transition-all ease-in duration-150 hover:scale-105"/>
           </Link>
           <Link href="/" className="font-medium text-sm text-primary dark:text-darkmode-primary flex justify-center items-center h-10 p-2 rounded-sm transition-all ease-in duration-150 hover:bg-hovercolor dark:hover:bg-darkmode-hovercolor">How To Use</Link>
           <Link href="/" className="font-medium text-sm text-primary dark:text-darkmode-primary flex justify-center items-center h-10 p-2 rounded-sm transition-all ease-in duration-150 hover:bg-hovercolor dark:hover:bg-darkmode-hovercolor">About Us</Link>
         </div>
-        <Link href="/">
-          <Button className="h-10 rounded-sm cursor-pointer dark:bg-white dark:hover:bg-[#c8c8c8]">Sign In</Button>
-        </Link>
+        <div className="flex flex-row items-center gap-x-6">
+          <DarkmodeToggle/>
+          <Link href="/">
+            <Button className="h-10 rounded-sm cursor-pointer dark:bg-white dark:hover:bg-[#c8c8c8]">Sign In</Button>
+          </Link>
+        </div>
       </div>
     </nav>
   )
