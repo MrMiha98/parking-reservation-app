1224919154bdff7722b2c72c57c79dcde0aab048
Miha <miha.skrjanec@gmail.com>
2025-06-05 15:45:41 +0200

    Minor Fixes

--- app/components/ParkingGrid.tsx
+++ app/components/ParkingGrid.tsx
@@ -8,47 +8,46 @@ const formattedToday = format(today);
 const formattedTomorrow = format(tomorrow);
 
 export default async function ParkingGrid() {
-  const { data, error } = await supabase.from("parking_availability").select("spot_id, date, name");
+  const { data, error } = await supabase.from("parking_availability").select("spot_id, date");
 
   if (error) {
     console.error(error);
   }
 
   const allSpots = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  const ownerNames = ["Bella C.", "Liam T.", "Emma R.", "Noah J.", "Olivia M.", "Mason K.", "Ava L.", "Ethan W.", "Sophia G.", "Lucas B.", "Isabella S.", "Logan D.", "Mia F.", "James H.", "Charlotte N.", "Benjamin A.", "Amelia P.", "Elijah V.", "Harper Z.", "Alexander Q."];
 
   return (
     <div className="w-full max-w-3xl mx-auto flex flex-col items-start p-10">
       <h1 className="font-bold text-3xl text-black dark:text-white mb-12">Reserve Your Parking Spot Now</h1>
       <h3 className="text-[#323232] dark:text-darkmode-primary">1st floor</h3>
-      <div className="h-[1px] w-full bg-border dark:bg-darkmode-border mt-1 mb-4"></div>
+      <div className="h-[0.5px] w-full bg-border dark:bg-darkmode-border mt-1 mb-4"></div>
       <div className="w-fit grid grid-cols-4 gap-x-2 gap-y-6">
         {allSpots.slice(0, 7).map(spot => {
           const availableToday = data?.find(el => el.spot_id == spot && el.date == formattedToday);
           const availableTommorow = data?.find(el => el.spot_id == spot && el.date == formattedTomorrow);
-          const name = availableToday ? data?.find(el => el.spot_id === spot)?.name: null;
 
           return (
-            <div key={spot} className={`${availableToday ? "bg-green-300" : "bg-red-300"} h-28 w-20 relative flex justify-center items-center`}>
+            <div key={spot} className={`${availableToday ? "bg-green-400" : "bg-red-400"} h-28 w-20 relative flex justify-center items-center ${spot == 5 ? "col-start-2" : ""} cursor-pointer`}>
               <h1 className="text-2xl font-bold text-gray-100">{spot}</h1>
-              <p className="text-primary text-sm font-semibold absolute bottom-0">{name}</p>
-              <div className={`absolute right-0 top-0 w-0 h-0 border-r-24 border-b-24 ${availableTommorow ? "border-green-500" : "border-red-500"} border-b-transparent`}></div>
+              <p className="text-primary text-[12px] font-semibold absolute bottom-0">{availableToday ? "" : ownerNames[spot - 1]}</p>
+              <div className={`absolute right-0 top-0 w-0 h-0 border-r-24 border-b-24 ${availableTommorow ? "border-green-700" : "border-red-700"} border-b-transparent`}></div>
             </div>
           )
         })}
       </div>
       <h3 className="text-[#323232] dark:text-darkmode-primary mt-16">2nd floor</h3>
-      <div className="h-[1px] w-full bg-border dark:bg-darkmode-border mt-1 mb-4"></div>
+      <div className="h-[0.5px] w-full bg-border dark:bg-darkmode-border mt-1 mb-4"></div>
       <div className="w-fit grid grid-cols-7 gap-x-2 gap-y-6">
         {allSpots.slice(7, 20).map(spot => {
           const availableToday = data?.find(el => el.spot_id == spot && el.date == formattedToday);
           const availableTommorow = data?.find(el => el.spot_id == spot && el.date == formattedTomorrow);
-          const name = availableToday ? data?.find(el => el.spot_id === spot)?.name: null;
 
           return (
-            <div key={spot} className={`${availableToday ? "bg-green-300" : "bg-red-300"} h-28 w-20 relative flex justify-center items-center ${spot == 8 ? "col-start-2" : ""}`}>
+            <div key={spot} className={`${availableToday ? "bg-green-400" : "bg-red-400"} h-28 w-20 relative flex justify-center items-center ${spot == 8 ? "col-start-2" : ""} cursor-pointer`}>
               <h1 className="text-2xl font-bold text-gray-100">{spot}</h1>
-              <p className="text-primary text-sm font-semibold absolute bottom-0">{name}</p>
-              <div className={`absolute right-0 top-0 w-0 h-0 border-r-24 border-b-24 ${availableTommorow ? "border-green-500" : "border-red-500"} border-b-transparent`}></div>
+              <p className="text-primary text-[12px] font-semibold absolute bottom-0">{availableToday ? "" : ownerNames[spot - 1]}</p>
+              <div className={`absolute right-0 top-0 w-0 h-0 border-r-24 border-b-24 ${availableTommorow ? "border-green-700" : "border-red-700"} border-b-transparent`}></div>
             </div>
           )
         })}
